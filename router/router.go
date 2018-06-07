17d900c8bdfbd1b48945543bcfb24017ba255420
Komei1009 <1994komei1009@gmail.com>
2018-06-07 13:53:58 +0900

    クロスオリジン対応

--- router/router.go
+++ router/router.go
@@ -1,18 +1,35 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
+	"time"
+
 	"github.com/HAL-RO-Developer/caseTeamB_server/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 
+	r.Use(cros)
 	api := r.Group("/api")
 	apiRouter(api)
 	auth := api.Group("")
 	auth.Use(middleware.Jwt("hogehoge", 3600*24*365))
 	authApiRouter(auth)
 	return r
+
+}
+
+func cros(c *gin.Context) {
+	headers := c.Request.Header.Get("Access-Control-Request-Headers")
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,PATCH,DELETE,OPTIONS")
+	c.Header("Access-Control-Allow-Headers", headers)
+	if c.Request.Method == "OPTIONS" {
+		c.Status(200)
+		c.Abort()
+	}
+	c.Set("start_time", time.Now())
+	c.Next()
 
 }
